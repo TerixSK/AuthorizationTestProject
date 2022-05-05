1a8209d63294b1bc721d7a7903147a5ead0bb4d0
TerixSK <46744578+TerixSK@users.noreply.github.com>
2022-05-05 14:22:23 +0300

    New feature: update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:authorization_test_project/authorization_feature/presentation/bloc/user_bloc/bloc/user_bloc.dart';
 import 'package:authorization_test_project/common/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'authorization_feature/locator_service.dart' as di;
 import 'authorization_feature/locator_service.dart';
-import 'authorization_feature/presentation/pages/authorization_page.dart';
+import 'authorization_feature/presentation/bloc/user_bloc/user_bloc.dart';
+import 'authorization_feature/presentation/pages/log_in_page.dart';
 
 Future<void> main() async {
   await di.init();
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         title: 'Authorization Test Project',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primaryColor: AppColors.purple),
-        home: const AuthorizationPage(),
+        home: LogInPage(),
       ),
     );
   }
