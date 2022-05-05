14ae7074c001d3f0178b97777de936a96b52347b
TerixSK <46744578+TerixSK@users.noreply.github.com>
2022-05-05 14:22:13 +0300

    New feature: update

--- lib/authorization_feature/locator_service.dart
+++ lib/authorization_feature/locator_service.dart
@@ -1,7 +1,7 @@
 import 'package:authorization_test_project/authorization_feature/data/datasources/user_local_datasource.dart';
 import 'package:authorization_test_project/authorization_feature/data/repositories/user_reposithory_impl.dart';
 import 'package:authorization_test_project/authorization_feature/domain/repositories/user_reposithory.dart';
-import 'package:authorization_test_project/authorization_feature/presentation/bloc/user_bloc/bloc/user_bloc.dart';
+import 'package:authorization_test_project/authorization_feature/presentation/bloc/user_bloc/user_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
